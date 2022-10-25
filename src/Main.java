334915a05e7501480f525f968298095ce8bdd3eb
Guto Silva <carlosac_silva@yahoo.com.br>
2022-10-25 12:12:15 -0300

    Criados outras classes.

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,6 @@
+import br.dev.guto.desafio.dominio.Bootcamp;
 import br.dev.guto.desafio.dominio.Curso;
+import br.dev.guto.desafio.dominio.Dev;
 import br.dev.guto.desafio.dominio.Mentoria;
 
 import java.time.LocalDate;
@@ -13,7 +15,7 @@ public class Main {
 
         Curso curso2 = new Curso();
 
-        curso2.setDescricao("POO com Java");
+        curso2.setTitulo("POO com Java");
         curso2.setCargaHoraria(40);
         curso2.setDescricao("Aprenda Orientação a Objetos com Java");
 
@@ -24,8 +26,33 @@ public class Main {
         mentoria.setData(LocalDate.now());
 
 
-        System.out.println(curso1);
-        System.out.println(curso2);
-        System.out.println(mentoria);
+//        System.out.println(curso1);
+//        System.out.println(curso2);
+//        System.out.println(mentoria);
+
+        Bootcamp bootcamp = new Bootcamp();
+        bootcamp.setNome("Bootcamp Java Developer");
+        bootcamp.setDescricao("Formação profissional para desenvolvedores Java");
+        bootcamp.getConteudos().add(curso1);
+        bootcamp.getConteudos().add(curso2);
+        bootcamp.getConteudos().add(mentoria);
+
+        Dev pernalonga = new Dev();
+        pernalonga.setNome("Pernalonga");
+        pernalonga.inscreverBootcamp(bootcamp);
+        System.out.println("Conteúdos Inscritos: " + pernalonga.getConteudosInscritos());
+        //pernalonga.progredir();
+        System.out.println("Conteúdos Concluídos: " + pernalonga.getConteudosConcluidos());
+        System.out.println("XP: " + pernalonga.calcularTotalXp());
+
+        System.out.println("--------------------------------------------------------------------------------");
+
+        Dev patolino = new Dev();
+        patolino.setNome("Patolino");
+        patolino.inscreverBootcamp(bootcamp);
+        System.out.println("Conteúdos Inscritos: " + patolino.getConteudosInscritos());
+        //patolino.progredir();
+        System.out.println("Conteúdos Concluídos: " + patolino.getConteudosConcluidos());
+        System.out.println("XP: " + patolino.calcularTotalXp());
     }
 }
